665c3b7604e5153a743721754878ded797d832b1
Rachel Forshee <forsheej@amazon.com>
2019-06-26 14:57:00 -0700

    Add Cooldowns module.

--- src/parser/jobs/gnb/modules/index.ts
+++ src/parser/jobs/gnb/modules/index.ts
@@ -1,9 +1,11 @@
 import Ammo from './Ammo'
 import Combos from './Combos'
+import Cooldowns from './Cooldowns'
 import NoMercy from './NoMercy'
 
 export default [
 	Ammo,
 	Combos,
+	Cooldowns,
 	NoMercy,
 ]
